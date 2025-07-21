5160722191d11e5c5f788a64f2ab49f837d0826b
ajea <smithajea@rocketmail.com>
2025-07-21 10:59:23 -0400

    tailwind / daisy ui / icons setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+<<<<<<< Updated upstream
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
+=======
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  // eslint: {
+  //   config: { standalone: false },
+  // },
+});
+>>>>>>> Stashed changes
