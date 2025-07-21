4847d57d987397c501310542d4fb66c74f743173
ajea <smithajea@rocketmail.com>
2025-07-21 11:04:43 -0400

    fix: merge issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-<<<<<<< Updated upstream
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
-=======
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/icon"],
@@ -15,8 +10,7 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
-  // eslint: {
-  //   config: { standalone: false },
-  // },
+  eslint: {
+    config: { standalone: false },
+  },
 });
->>>>>>> Stashed changes
