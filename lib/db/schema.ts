e001471c9ca0cabc86e4e053f92e17fe63c2777e
AjeaS <Smithajea@rocketmail.com>
2025-07-22 16:04:03 -0400

    auth setup (#54)
    * auth setup
    
    * add env to yml file

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,4 +1,60 @@
-import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const user = sqliteTable("user", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  email: text().notNull().unique(),
+  emailVerified: integer({ mode: "boolean" })
+    .$defaultFn(() => false)
+    .notNull(),
+  image: text(),
+  createdAt: integer()
+    .notNull(),
+  updatedAt: integer()
+    .notNull(),
+});
+
+export const session = sqliteTable("session", {
+  id: int().primaryKey({ autoIncrement: true }),
+  expiresAt: integer().notNull(),
+  token: text().notNull().unique(),
+  createdAt: integer().notNull(),
+  updatedAt: integer().notNull(),
+  ipAddress: text(),
+  userAgent: text(),
+  userId: text()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+});
+
+export const account = sqliteTable("account", {
+  id: int().primaryKey({ autoIncrement: true }),
+  accountId: text().notNull(),
+  providerId: text().notNull(),
+  userId: text()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  accessToken: text(),
+  refreshToken: text(),
+  idToken: text(),
+  accessTokenExpiresAt: integer(),
+  refreshTokenExpiresAt: integer(),
+  scope: text(),
+  password: text(),
+  createdAt: integer().notNull(),
+  updatedAt: integer().notNull(),
+});
+
+export const verification = sqliteTable("verification", {
+  id: int().primaryKey({ autoIncrement: true }),
+  identifier: text().notNull(),
+  value: text().notNull(),
+  expiresAt: integer().notNull(),
+  createdAt: integer(),
+  updatedAt: integer(),
+});
+
+// -------- LOCATION SCHEMA --------
 
 export const location = sqliteTable("location", {
   id: int().primaryKey({ autoIncrement: true }),
@@ -9,9 +65,9 @@ export const location = sqliteTable("location", {
   long: real().notNull(),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
-  //   userId: integer("user_id")
-  //     .notNull()
-  //     .references(() => usersTable.id, { onDelete: "cascade" }),
+  userId: integer()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
 });
 export const locationLog = sqliteTable("locationLog", {
   id: int().primaryKey({ autoIncrement: true }),
@@ -24,9 +80,9 @@ export const locationLog = sqliteTable("locationLog", {
   locationId: int().notNull().references(() => location.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
-  //   userId: integer("user_id")
-  //     .notNull()
-  //     .references(() => usersTable.id, { onDelete: "cascade" }),
+  userId: integer()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
 });
 export const locationLogImage = sqliteTable("locationLogImage", {
   id: int().primaryKey({ autoIncrement: true }),
@@ -34,7 +90,7 @@ export const locationLogImage = sqliteTable("locationLogImage", {
   locationLogId: int().notNull().references(() => locationLog.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
-  //   userId: integer("user_id")
-  //     .notNull()
-  //     .references(() => usersTable.id, { onDelete: "cascade" }),
+  userId: integer()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
 });
