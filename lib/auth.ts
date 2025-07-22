288d231aa4e9b3f364bef621fcd973c6422cf439
ajea <smithajea@rocketmail.com>
2025-07-22 15:53:16 -0400

    auth setup

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+// Hooks up better-auth with db (with drizzle)
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import db from "./db/index";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+  }),
+  advanced: {
+    generateId: false,
+  },
+});
