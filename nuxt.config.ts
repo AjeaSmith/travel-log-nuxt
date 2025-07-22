4352802aea986d0692db724680e4b0344485b0ab
ajea <smithajea@rocketmail.com>
2025-07-22 01:12:09 -0400

    typesafe env setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import "./lib/env";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
